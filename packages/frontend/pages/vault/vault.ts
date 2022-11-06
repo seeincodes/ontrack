86cbb740ffcbcd4fc42e6b12d2a624fdb145565f
“seeinplays” <seeinplays@gmail.com>
2022-11-05 22:42:57 -0600

    vault and wallet creation

--- packages/frontend/pages/vault/vault.ts
+++ packages/frontend/pages/vault/vault.ts
@@ -1,14 +1,38 @@
 import Triangle from "triangle";
 
-export default function Vault() {
+function Vault() {
     const triangle = new Triangle(process.env.TRIANGLE_KEY);
 
     const vault = await triangle.vaults.create({
-        name: "Test Value",
+        name: "Ontrack Wallet",
+    });
+
+    const ethereumWallet = await triangle.wallets.create({
+        name: vault.name,
+        network: "ethereum_goerli",
+        vault: vault.id,
+    });
+
+    const polygonWallet = await triangle.wallets.create({
+        name: vault.name,
+        network: "polygon_mumbai",
+        vault: vault.id,
+    });
+
+    const optimismWallet = await triangle.wallets.create({
+        name: vault.name,
+        network: "optimism_goerli",
+        vault: vault.id,
     });
 
     const vaults = await triangle.vaults.list();
 
 
-    return <div>Vault < /div>
+    return (
+        <div>Vault < /div>
+        div
+
+    )
 }
+
+export default Vault;
